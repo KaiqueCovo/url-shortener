c593c6d22e79ec9836dc2c9495efa23334598cf3
Kaique Covo <kaique.kng@gmail.com>
2022-06-13 12:25:52 -0300

    feat(components): add icon components

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,31 @@
+import { createIcon } from '@chakra-ui/react';
+
+const CopyIcon = createIcon({
+  displayName: 'CopyIcon',
+  defaultProps: {
+    fill: 'none',
+    stroke: 'currentColor',
+    strokeWidth: '2',
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+  },
+  path: (
+    <>
+      <rect x='9' y='9' width='13' height='13' rx='2' ry='2' />
+      <path d='M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1' />
+    </>
+  ),
+});
+
+const CloseIcon = createIcon({
+  displayName: 'CloseIcon',
+  defaultProps: {
+    stroke: 'currentColor',
+    strokeWidth: '2',
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+  },
+  path: <path d='M18 6 6 18M6 6l12 12' />,
+});
+
+export { CopyIcon, CloseIcon };
