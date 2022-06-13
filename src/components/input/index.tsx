50c31999f18653791e6d7736c13fc0a458c2bed1
Kaique Covo <kaique.kng@gmail.com>
2022-06-13 12:30:21 -0300

    refactor(components): change focus border color

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { InputProps, Input as ChakraInput } from '@chakra-ui/react';
 
 export const Input = (props: InputProps): React.ReactElement => (
-  <ChakraInput {...props} />
+  <ChakraInput {...props} focusBorderColor='primary' />
 );
