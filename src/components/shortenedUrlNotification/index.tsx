85051609008c6d8281a046e2360a708751caeb9f
Kaique Covo <kaique.kng@gmail.com>
2022-06-15 15:10:24 -0300

    feat(shortenedurlnotification): add function handle copy url

--- src/components/shortenedUrlNotification/index.tsx
+++ src/components/shortenedUrlNotification/index.tsx
@@ -11,41 +11,55 @@ interface IShortenedUrlNotificationProps {
 export const ShortenedUrlNotification = ({
   originalUrl,
   shortenedUrl,
-}: IShortenedUrlNotificationProps): React.ReactElement => (
-  <Flex
-    direction={{ base: 'column', md: 'row' }}
-    gap={{ base: 4, sm: 8 }}
-    alignItems={{ base: 'start', sm: 'center' }}
-    background={'primary'}
-    justifyContent={'space-between'}
-    color={'white'}
-    p={4}
-    borderRadius={10}
-  >
-    <Text textAlign={'start'} fontSize={'sm'} fontWeight={700} noOfLines={1}>
-      {originalUrl}
-    </Text>
+}: IShortenedUrlNotificationProps): React.ReactElement => {
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(shortenedUrl)
+      .then(() => {
+        console.log(`${shortenedUrl} was copied to clipboard.`);
+      })
+      .catch((err) => {
+        console.error(`Error copying text to clipboard: ${err}`);
+      });
+  };
+
+  return (
     <Flex
+      background={'primary'}
+      justifyContent={'space-between'}
+      color={'white'}
+      p={4}
+      borderRadius={10}
       direction={{ base: 'column', md: 'row' }}
       gap={{ base: 4, sm: 8 }}
-      width={'100%'}
-      justifyContent={'flex-end'}
       alignItems={{ base: 'start', sm: 'center' }}
     >
-      <Text fontSize={'sm'} fontWeight={700}>
-        {shortenedUrl}
+      <Text textAlign={'start'} fontSize={'sm'} fontWeight={700} noOfLines={1}>
+        {originalUrl}
       </Text>
-      <Button
-        fontWeight={700}
-        bg={'orange.300'}
-        width={{ base: '100%', sm: 'auto' }}
-        _active={{ bg: 'orange.400' }}
-        _hover={{
-          bg: 'orange.400',
-        }}
+      <Flex
+        width={'100%'}
+        justifyContent={'flex-end'}
+        direction={{ base: 'column', md: 'row' }}
+        gap={{ base: 4, sm: 8 }}
+        alignItems={{ base: 'start', sm: 'center' }}
       >
-        <Icon as={CopyIcon} w={6} h={6} />
-      </Button>
+        <Text fontSize={'sm'} fontWeight={700}>
+          {shortenedUrl}
+        </Text>
+        <Button
+          fontWeight={700}
+          bg={'orange.300'}
+          width={{ base: '100%', sm: 'auto' }}
+          _active={{ bg: 'orange.400' }}
+          _hover={{
+            bg: 'orange.400',
+          }}
+          onClick={handleCopy}
+        >
+          <Icon as={CopyIcon} w={6} h={6} />
+        </Button>
+      </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
